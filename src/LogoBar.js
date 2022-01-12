83941da36ad6d44471d9f36ccd80304d5f3ca15c
jbrennan414 <brennanj414@gmail.com>
2022-01-11 20:42:08 -0700

    first commit

---
+++ src/LogoBar.js
@@ -0,0 +1,36 @@
+import styled, { css }from 'styled-components';
+import coffee_logo from './assets/coffee_logo.png';
+import shopping_bag from './assets/shopping_bag.png'; 
+import hamburger_menu from './assets/hamburger_menu.png';
+
+function LogoBar() {
+
+    const Bar = styled.div`
+      width: 100%;
+      height: 100px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      padding: 5px;
+    `
+
+    const SmallIcons = styled.img`
+      height: 25px;
+      width: 25px;
+    `
+    
+    const LargeIcon = styled.img`
+    height: 75px;
+    width: 75px;
+    `
+
+  return (
+    <Bar>
+      <SmallIcons src={hamburger_menu} alt="hamburger menu" />
+      <LargeIcon src={coffee_logo} alt="logo" />
+      <SmallIcons src={shopping_bag} alt="shopping bag" />
+    </Bar>
+  );
+}
+
+export default LogoBar;
